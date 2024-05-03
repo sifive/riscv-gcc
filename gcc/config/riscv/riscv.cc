3459b1176eba09a1be499c5834cc30abcf78618c
Monk Chiang <monk.chiang@sifive.com>
2024-05-03 09:21:15 +0800

    SiFive: Add no_cfi_ss attribute and -mcfi-ss option for disable zicfiss.

--- gcc/config/riscv/riscv.cc
+++ gcc/config/riscv/riscv.cc
@@ -202,6 +202,9 @@ struct GTY(())  machine_function {
   /* True if RA must be saved because of a far jump.  */
   bool far_jump_used;
 
+  /* True if current function disable CFI shadow stack.  */
+  bool no_cfi_ss_p;
+
   /* The current frame information, calculated by riscv_compute_frame_info.  */
   struct riscv_frame_info frame;
 
@@ -661,6 +664,9 @@ static const attribute_spec riscv_gnu_attributes[] =
   /* This attribute generates prologue/epilogue for interrupt handlers.  */
   {"interrupt", 0, 1, false, true, true, false, riscv_handle_type_attribute,
    NULL},
+  /* The attribute disable CFI shadow stack.  */
+  { "no_cfi_ss", 0, 0, true, false, false, false,
+    riscv_handle_fndecl_attribute, NULL },
 
   /* The following two are used for the built-in properties of the Vector type
      and are not used externally */
@@ -6657,6 +6663,16 @@ riscv_naked_function_p (tree func)
   return NULL_TREE != lookup_attribute ("naked", DECL_ATTRIBUTES (func_decl));
 }
 
+/* Return true if FUNC disable CFI shadow stack.  */
+static bool
+riscv_no_cfi_ss_p (tree func)
+{
+  tree func_decl = func;
+  if (func == NULL_TREE)
+    func_decl = current_function_decl;
+  return NULL_TREE != lookup_attribute ("no_cfi_ss", DECL_ATTRIBUTES (func_decl));
+}
+
 /* Implement TARGET_ALLOCATE_STACK_SLOTS_FOR_ARGS.  */
 static bool
 riscv_allocate_stack_slots_for_args ()
@@ -11229,6 +11245,7 @@ riscv_set_current_function (tree decl)
       cfun->machine->naked_p = riscv_naked_function_p (decl);
       cfun->machine->interrupt_handler_p
 	= riscv_interrupt_type_p (TREE_TYPE (decl));
+      cfun->machine->no_cfi_ss_p = riscv_no_cfi_ss_p (decl);
 
       if (cfun->machine->naked_p && cfun->machine->interrupt_handler_p)
 	error ("function attributes %qs and %qs are mutually exclusive",
@@ -14017,7 +14034,12 @@ expand_reversed_crc_using_clmul (scalar_mode crc_mode, scalar_mode data_mode,
 bool is_zicfiss_p ()
 {
   if (TARGET_ZICFISS && (flag_cf_protection & CF_RETURN))
-    return true;
+    {
+      if (cfun && cfun->machine->no_cfi_ss_p)
+	return false;
+      else
+	return true;
+    }
 
   return false;
 }
