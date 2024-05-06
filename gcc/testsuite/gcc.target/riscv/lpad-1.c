1288add91b8f3f0c74a5b86dfafb016bb652390a
Monk Chiang <monk.chiang@sifive.com>
2024-05-06 14:07:50 +0800

    SiFive: Add no_cfi_lp attribute and -mcfi-lp option for disable zicfilp.

---
+++ gcc/testsuite/gcc.target/riscv/lpad-1.c
@@ -0,0 +1,8 @@
+/* Verify the no_cfi_lp attribute.  */
+/* { dg-do compile { target { riscv64*-*-* } } } */
+/* { dg-options "-O0 -march=rv64gc_zicfilp -mabi=lp64d" } */
+
+__attribute__((no_cfi_lp))
+int foo (void) { return 42; }
+
+/* { dg-final { scan-assembler-not "lpad\t" } } */
