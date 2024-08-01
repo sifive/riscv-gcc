2485914d6857dd6bc280edaab1ad5069fd47a9cf
Monk Chiang <monk.chiang@sifive.com>
2024-08-01 11:06:28 +0800

    SiFive: Avoid generate sibcall when cmodel is compact.
    Note:
      The t1 register is reused for allocate vector stack, generate
    got_gprel and sibcall. We don't have register for sibcall.

--- gcc/config/riscv/riscv.cc
+++ gcc/config/riscv/riscv.cc
@@ -10405,6 +10405,9 @@ riscv_function_ok_for_sibcall (tree decl ATTRIBUTE_UNUSED,
   if (riscv_cmodel == CM_LARGE)
     return false;
 
+  if (riscv_cmodel == CM_COMPACT)
+    return false;
+
   return true;
 }
 
