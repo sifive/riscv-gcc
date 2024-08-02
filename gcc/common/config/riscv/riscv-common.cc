822b59dccf90ef204684718dd1db47a7dce8d834
Hau Hsu <hau.hsu@sifive.com>
2024-08-02 10:13:03 +0800

    SiFive: Rename svkt to svukte (v0.3)
    Jira: SCT-4007

--- gcc/common/config/riscv/riscv-common.cc
+++ gcc/common/config/riscv/riscv-common.cc
@@ -509,7 +509,7 @@ static const struct riscv_ext_version riscv_ext_version_table[] =
   {"sswg",  ISA_SPEC_CLASS_UNRATIFIED, 0, 3},
   {"ssqosid",  ISA_SPEC_CLASS_NONE, 1, 0},
   {"ssstrict", ISA_SPEC_CLASS_NONE, 1, 0},
-  {"skvt", ISA_SPEC_CLASS_UNRATIFIED, 0, 2},
+  {"svukte",   ISA_SPEC_CLASS_NONE, 0, 3},
 
   {"v",       ISA_SPEC_CLASS_UNRATIFIED, 0, 10},
   {"zvlsseg", ISA_SPEC_CLASS_NONE, 1, 0},
