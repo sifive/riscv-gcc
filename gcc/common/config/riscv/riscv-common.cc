630f6dd53af9c5f58ed21cffa4f7af6801aa2d85
Monk Chiang <monk.chiang@sifive.com>
2025-03-27 20:50:35 +0800

    SiFive: Add Zvfofp8min 0.2 extension

--- gcc/common/config/riscv/riscv-common.cc
+++ gcc/common/config/riscv/riscv-common.cc
@@ -197,6 +197,7 @@ static const riscv_implied_info_t riscv_implied_info[] =
   {"zvfbfmin", "zve32f"},
   {"zvfbfwma", "zvfbfmin"},
   {"zvfbfwma", "zfbfmin"},
+  {"zvfofp8min", "zve32f"},
   {"zvfhmin", "zve32f"},
   {"zvfh", "zve32f"},
   {"zvfh", "zfhmin"},
@@ -536,6 +537,7 @@ static const struct riscv_ext_version riscv_ext_version_table[] =
 
   {"zvfbfmin", ISA_SPEC_CLASS_UNRATIFIED, 0, 8},
   {"zvfbfwma", ISA_SPEC_CLASS_UNRATIFIED, 0, 8},
+  {"zvfofp8min", ISA_SPEC_CLASS_NONE, 0, 2},
 
   {"zvkns",  ISA_SPEC_CLASS_UNRATIFIED, 0, 1},
   {"zvknf",  ISA_SPEC_CLASS_UNRATIFIED, 0, 1},
@@ -1961,6 +1963,7 @@ static const riscv_ext_flag_table_t riscv_ext_flag_table[] =
   RISCV_EXT_FLAG_ENTRY ("zvfbfwma", x_riscv_zf_subext, MASK_ZVFBFWMA),
   RISCV_EXT_FLAG_ENTRY ("zvfhmin",  x_riscv_zf_subext, MASK_ZVFHMIN),
   RISCV_EXT_FLAG_ENTRY ("zvfh",     x_riscv_zf_subext, MASK_ZVFH),
+  RISCV_EXT_FLAG_ENTRY ("zvfofp8min", x_riscv_zf_subext, MASK_ZVFOFP8MIN),
 
   RISCV_EXT_FLAG_ENTRY ("zfa", x_riscv_zfa_subext, MASK_ZFA),
 
