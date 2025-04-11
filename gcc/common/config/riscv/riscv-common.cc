7c258be296d46551d88aa670cb9a9569693d3c34
Jerry Zhang Jian <jerry.zhangjian@sifive.com>
2025-04-11 06:03:35 -0700

    SiFive: update Mammoth to 0.6.1
    Signed-off-by: Jerry Zhang Jian <jerry.zhangjian@sifive.com>

--- gcc/common/config/riscv/riscv-common.cc
+++ gcc/common/config/riscv/riscv-common.cc
@@ -243,22 +243,31 @@ static const riscv_implied_info_t riscv_implied_info[] =
   {"xsfvfexp16e", "zvfh"},
   {"xsfvfbfexp16e", "zve32f"},
   {"xsfvfnrclipxfqf", "zve32f"},
-  /* Mammoth */
-  {"xsfmmbase", "v"},
+  /* Mammoth 0.6.1 */
+  {"xsfmmbase", "zve32x"},
   {"xsfmm32a4i", "xsfmmbase"},
   {"xsfmm32a8i", "xsfmmbase"},
   {"xsfmm32a8f", "xsfmmbase"},
+  {"xsfmm32a8f", "zve32f"},
   {"xsfmm32a16f", "xsfmmbase"},
+  {"xsfmm32a16f", "zve32f"},
   {"xsfmm32a32f", "xsfmmbase"},
+  {"xsfmm32a32f", "zve32f"},
   {"xsfmm32a", "xsfmmbase"},
+  {"xsfmm32a", "zve32f"},
   {"xsfmm32a", "xsfmm32a8i"},
   {"xsfmm32a", "xsfmm32a16f"},
   {"xsfmm32a", "xsfmm32a32f"},
   {"xsfmm64a64f", "xsfmmbase"},
+  {"xsfmm64a64f", "zve64d"},
   {"xsfmm128t", "xsfmmbase"},
+  {"xsfmm128t", "zvl512b"},
   {"xsfmm64t", "xsfmmbase"},
+  {"xsfmm64t", "zvl256b"},
   {"xsfmm32t", "xsfmmbase"},
+  {"xsfmm32t", "zvl128b"},
   {"xsfmm16t", "xsfmmbase"},
+  {"xsfmm16t", "zvl64b"},
 
   {NULL, NULL}
 };
