04a26d408111d5791844c1264481cea15871c7b4
Jerry Zhang Jian <jerry.zhangjian@sifive.com>
2025-04-16 10:47:00 -0700

    SiFive: resolve conflicts around 689b0f8
    - The arch-41.c is added by upstream while downstream also added
      it in "SiFive: Add Zalasr extension", rename the downstream test
      to arch-42.c
    
    Signed-off-by: Jerry Zhang Jian <jerry.zhangjian@sifive.com>

--- gcc/testsuite/gcc.target/riscv/arch-41.c
+++ gcc/testsuite/gcc.target/riscv/arch-41.c
@@ -1,14 +1,7 @@
 /* { dg-do compile } */
-<<<<<<< HEAD
-/* { dg-options "-march=rv64i_zalasr -mabi=lp64" } */
-int foo ()
-{
-}
-=======
 /* { dg-options "-march=rv64i -mabi=lp64d" } */
 int
 foo ()
 {}
 
 /* { dg-error "requested ABI requires '-march' to subsume the 'D' extension" "" { target *-*-* } 0 } */
->>>>>>> refs/rewritten/420aef66461965586293516945c6f5dcfda911ed-2
