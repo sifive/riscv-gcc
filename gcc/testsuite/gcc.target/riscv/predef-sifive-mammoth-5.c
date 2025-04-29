4183b40a9463384a11183975fb660bc86a5da2aa
Jerry Zhang Jian <jerry.zhangjian@sifive.com>
2025-04-29 01:09:38 -0700

    SiFive: add Mammoth 0.6.1 ISA imply rule tests
    Signed-off-by: Jerry Zhang Jian <jerry.zhangjian@sifive.com>

---
+++ gcc/testsuite/gcc.target/riscv/predef-sifive-mammoth-5.c
@@ -0,0 +1,47 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv64i_xsfmm32a16f0p6 -mabi=lp64 -mcmodel=medlow -Wno-riscv-implicit-extension-version" } */
+
+int main () {
+
+#ifndef __riscv_arch_test
+#error "__riscv_arch_test"
+#endif
+
+#if __riscv_xlen != 64
+#error "__riscv_xlen"
+#endif
+
+#if !defined(__riscv_i)
+#error "__riscv_i"
+#endif
+
+#if !defined(__riscv_f)
+#error "__riscv_f"
+#endif
+
+#if !defined(__riscv_zicsr)
+#error "__riscv_zicsr"
+#endif
+
+#if !defined(__riscv_zve32f)
+#error "__riscv_zve32f"
+#endif
+
+#if !defined(__riscv_zve32x)
+#error "__riscv_zve32x"
+#endif
+
+#if !defined(__riscv_zvl32b)
+#error "__riscv_zvl32b"
+#endif
+
+#if !defined(__riscv_xsfmm32a16f)
+#error "__riscv_xsfmm32a16f"
+#endif
+
+#if !defined(__riscv_xsfmmbase)
+#error "__riscv_xsfmmbase"
+#endif
+
+  return 0;
+}
