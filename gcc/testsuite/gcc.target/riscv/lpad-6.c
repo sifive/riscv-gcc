38e070e240da5ce2439b6adb7a8298d2aed9c853
Kito Cheng <kito.cheng@sifive.com>
2025-05-14 16:43:33 +0800

    SiFive: Switch to unlabeled scheme

--- gcc/testsuite/gcc.target/riscv/lpad-6.c
+++ gcc/testsuite/gcc.target/riscv/lpad-6.c
@@ -1,6 +1,6 @@
 /* Verify the -fcf-protection=branch.  */
 /* { dg-do compile { target { riscv64*-*-* } } } */
-/* { dg-options "-O0 -march=rv64gc_zicfilp -mabi=lp64d -fcf-protection=branch" } */
+/* { dg-options "-O0 -march=rv64gc_zicfilp -mabi=lp64d -fcf-protection=branch -mcf-branch-label-scheme=fixed-one" } */
 
 int foo (void) { return 42; }
 
