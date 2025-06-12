b4a71894cb8b37dfc531491c9d2c9078a0588ad5
Monk Chiang <monk.chiang@sifive.com>
2025-06-12 06:45:29 -0700

    Sifive: Skip xthead test.

--- gcc/testsuite/gcc.target/riscv/pr116131.c
+++ gcc/testsuite/gcc.target/riscv/pr116131.c
@@ -1,4 +1,5 @@
 /* { dg-do compile { target { ! riscv_abi_e } } } */
+/* { dg-skip-if "Skipped due to SiFive does not support xthead" { riscv*-*-* } } */
 /* { dg-skip-if "" { *-*-* } { "-flto" "-O0" "-Og" "-Os" "-Oz" } } */
 /* { dg-options "-march=rv64gc_xtheadmemidx" { target { rv64 } } } */
 /* { dg-options "-march=rv32gc_xtheadmemidx" { target { rv32 } } } */
