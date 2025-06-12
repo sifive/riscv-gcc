72e5de2f307e272a3350f4658679b7bc46d80073
Monk Chiang <monk.chiang@sifive.com>
2025-06-12 06:24:13 -0700

    SiFive: Update test for sifive mammoth.

--- gcc/testsuite/gcc.target/riscv/predef-sifive-mammoth-7.c
+++ gcc/testsuite/gcc.target/riscv/predef-sifive-mammoth-7.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-march=rv64i_xsfmm32a0p6 -mabi=lp64 -mcmodel=medlow -Wno-riscv-implicit-extension-version" } */
+/* { dg-options "-march=rv64im_xsfmm32a0p6 -mabi=lp64 -mcmodel=medlow -Wno-riscv-implicit-extension-version" } */
 
 int main () {
 
