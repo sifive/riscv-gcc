79143c622910dc097c8fa669df1c167febda7441
Monk Chiang <monk.chiang@sifive.com>
2025-06-14 05:53:03 -0700

    SiFive: Skip pr90838-2.c on RV32 due to missing CTZ optimization
    Note: The lack of CTZ lowering in RV32 backends may contribute to
    performance regressions observed in CoreMark benchmarks (e.g., up to -8% on
    SiFive E6 series).

--- gcc/testsuite/gcc.dg/pr90838-2.c
+++ gcc/testsuite/gcc.dg/pr90838-2.c
@@ -1,4 +1,5 @@
 /* { dg-do compile { target { ! riscv_abi_e } } } */
+/* { dg-skip-if "RV32 backend does not lower to CTZ due to missing pattern or codegen" { rv32 } } */
 /* { dg-options "-O2 -fdump-tree-forwprop2-details" } */
 /* { dg-additional-options "-mbmi" { target { { i?86-*-* x86_64-*-* } && { ! { ia32 } } } } } */
 /* { dg-additional-options "-march=rv64gc_zbb" { target { rv64 } } } */
