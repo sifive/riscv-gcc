1aaf8265f3c169b78d4f1386ef7db68d30cd2362
Jerry Zhang Jian <jerry.zhangjian@sifive.com>
2025-06-17 19:38:33 -0700

    SiFive: add tests for compact code model deprecation warning
    Signed-off-by: Jerry Zhang Jian <jerry.zhangjian@sifive.com>

---
+++ gcc/testsuite/gcc.target/riscv/compact-deprecated-warning-disabled.c
@@ -0,0 +1,8 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv64i -mabi=lp64 -mcmodel=compact -Wdeprecated -Wno-riscv-deprecated" } */
+
+int main() {
+  return 0;
+}
+
+/* { dg-bogus "-mcode-model=compact. is deprecated" "" { target riscv*-*-* } 0 } */
