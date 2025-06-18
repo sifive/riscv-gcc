4cd49e4212ca9b3e5974f461b59acf27cc275505
Jerry Zhang Jian <jerry.zhangjian@sifive.com>
2025-06-17 19:35:48 -0700

    SiFive: default disable compact code model depercation message
    Signed-off-by: Jerry Zhang Jian <jerry.zhangjian@sifive.com>

--- gcc/config/riscv/riscv.cc
+++ gcc/config/riscv/riscv.cc
@@ -10197,7 +10197,7 @@ riscv_option_override (void)
     }
 
   if (riscv_cmodel == CM_COMPACT)
-    warning (OPT_Wdeprecated,
+    warning (OPT_Wriscv_deprecated,
 	     "%<-mcode-model=compact%> is deprecated; "
 	     "Please migrate to %<-mcode-model=large%>");
 
