23fd0443fc458a02e06afe08ff990d9c6772c871
Monk Chiang <monk.chiang@sifive.com>
2025-07-30 10:47:24 +0800

    SiFive: Add p extension for march

--- gcc/common/config/riscv/riscv-common.cc
+++ gcc/common/config/riscv/riscv-common.cc
@@ -493,6 +493,7 @@ static const struct riscv_ext_version riscv_ext_version_table[] =
   {"xsfvfnrclipxfqf", ISA_SPEC_CLASS_NONE, 1, 0},
 
   /* SiFive */
+  {"p", ISA_SPEC_CLASS_NONE,     0, 15},
   {"e", ISA_SPEC_CLASS_NONE,     1, 9},
 
   {"sdext", ISA_SPEC_CLASS_NONE, 1, 0},
