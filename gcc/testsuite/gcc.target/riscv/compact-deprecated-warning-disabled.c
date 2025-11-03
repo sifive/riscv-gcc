f3fec4673bd4344625088c889cd429e1b394d849
Jerry Zhang Jian <jerry.zhangjian@sifive.com>
2025-11-03 14:14:44 +0800

    SiFive: Enable compact code model deprecation warning by default (#334)
    Enable the compact code model deprecation warning by default and provide
    -Wno-riscv-deprecated flag to suppress it when needed (e.g., for testing).
    
    Signed-off-by: Jerry Zhang Jian <jerry.zhangjian@sifive.com>

--- gcc/testsuite/gcc.target/riscv/compact-deprecated-warning-disabled.c
+++
@@ -1,8 +0,0 @@
-/* { dg-do compile } */
-/* { dg-options "-march=rv64i -mabi=lp64 -mcmodel=compact -Wdeprecated -Wno-riscv-deprecated" } */
-
-int main() {
-  return 0;
-}
-
-/* { dg-bogus "-mcmodel=compact. is deprecated" "" { target riscv*-*-* } 0 } */
