e1451fb50b03aee8c6e17496d380a799af6f11d3
Jerry Zhang Jian <jerry.zhangjian@sifive.com>
2025-12-15 19:06:02 -0800

    SiFive: Add -Wno-riscv-implicit-extension-version for zvqdotq test
    The zvqdotq extension has a dependency on zve32x. When GCC expands
    implied extensions, the reconstructed march string loses the version
    number for zvqdotq, triggering an implicit version warning.
    
    Add -Wno-riscv-implicit-extension-version to suppress this warning.
    TODO: This is likely a bug in arch-canonicalize that should be fixed
    to preserve version numbers when expanding implied extensions.
    
    Signed-off-by: Jerry Zhang Jian <jerry.zhangjian@sifive.com>

--- gcc/testsuite/gcc.target/riscv/arch-zvqdotq.c
+++ gcc/testsuite/gcc.target/riscv/arch-zvqdotq.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-march=rv64im_zvqdotq0p0 -mabi=lp64" } */
+/* { dg-options "-march=rv64im_zvqdotq0p0 -mabi=lp64 -Wno-riscv-implicit-extension-version" } */
 int foo()
 {
 }
